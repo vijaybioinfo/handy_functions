c0d6f27db9c58e0f596ae3a8f51935d84913af43
Vijay Lab <ciro@lji.org>
2022-02-01 15:32:06 +0100

    mount into master

---
+++ bash/mount.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+
+# ------------------------------------------------------------------------------
+# title: Mounting.
+# purpose: This script helps you mount/unmount.
+# author:
+#   - name: Ciro Ramírez-Suástegui
+#     email: cramsuig@gmail.com
+# date: 2022-01-12
+# ------------------------------------------------------------------------------
+
+# If Mac: http://osxfuse.github.io install FUSE and SSHFS
+if [[ ! -s $(which sshfs) ]]; then
+  echo "sudo apt-get install sshfs"
+else
+  echo "SSHFS present: $(which sshfs)"
+fi
+
+echo "Might need to write your local password"
+USERNAME=ciro.suastegui
+DIRNAME=/Volumes/mdrive
+REMNAME=${USERNAME}@vicb-submit-02:/home/icb/${USERNAME}
+
+ls -hola /Volumes
+# if [[ ! -z "$(ls -A ${DIRNAME})" ]]; then
+if [[ -d ${DIRNAME} ]]; then
+  echo "\033[0;31mMounted\033[0m, or dir exists, would you like to unmount (y/n)?"
+  read -p "" choice
+  case "$choice" in
+    y|Y ) sudo umount ${DIRNAME};;
+    n|N ) echo "\033[0;34mCool\033[0m";;
+    * ) echo "invalid";;
+  esac
+else
+  sudo mkdir -p ${DIRNAME}
+  echo "\033[0;32mWrite your remote password\033[0m"
+  sudo sshfs -o allow_other,default_permissions ${REMNAME} ${DIRNAME}
+fi
+if [[ -d ${DIRNAME} ]]; then
+  echo "\033[0;32mContent:\033[0m"
+  ls -hol ${DIRNAME}/;
+fi
